d80725b7a77f88c43e99518c405372dd712b602e
Rajendra Kumar <rajendrakbanjara@gmail.com>
2024-03-10 10:46:42 +0530

    API fetched

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./components/Profiles";
+import { useEffect, useState } from "react";
+
 function App() {
+  //code for API calling
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3031/profiles");
+      const details = await response.json();
+      console.log(details);
+      setData(details);
+    };
+    fetchData();
+  }, []);
+
+  const object = {
+    msg: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+  };
   return (
     <div className="App">
       <div className="main-container bg-slate-100 flex justify-center h-screen items-center">
         <div className="notification-container w-[43%] bg-white p-5 rounded-xl">
           <div className="heading flex w-full justify-between">
-            <p className=""><span className="font-bold text-xl w-auto">Notification </span><span className="bg-blue-900 px-2 text-white rounded-md">3</span></p>
-            <p className="hover:text-blue-900 text-gray-600">Mark all as read</p>
+            <p className="">
+              <span className="font-bold text-xl w-auto">Notification </span>
+              <span className="bg-blue-900 px-2 text-white rounded-md">3</span>
+            </p>
+            <p className="hover:text-blue-900 text-gray-600">
+              Mark all as read
+            </p>
           </div>
+          { data.length > 0 &&
           <div className="profiles pt-5">
-            <Profile name={"Mark Webber"} src="/assets/images/avatar-mark-webber.webp" activity={"reacted to your recent post"} status={"My first tournament today!"} time={"1m ago"} msg={""}/>
-            <Profile name={"Angela Gray"} src="/assets/images/avatar-angela-gray.webp" activity={"followed you"} status={""} time={"5m ago"} msg={""}/>
-            <Profile name={"Jacob Thompson"} src="/assets/images/avatar-jacob-thompson.webp" activity={"has joined your group"} status={""} club={"Chess Club"}time={""} msg={""}/>
-            <Profile name={"Rizky Hasanuddin"} src="/assets/images/avatar-rizky-hasanuddin.webp" activity={"sent you a private message"} status={""} time={"5 days ago"} msg={"Hello thanks"}/>
-            <Profile name={"Kimberly Smith"} src="/assets/images/avatar-kimberly-Smith.webp" activity={"commented on your picture"} status={""} time={"1 week ago"} msg={""} src2="/assets/images/image-chess.webp"/>
-            <Profile name={"Nathan Peterson"} src="/assets/images/avatar-nathan-peterson.webp" activity={"reacted to your recent post"} status={"5 end-game strategies to increase your win rate"} time={"2 weeks ago"} msg={""}/>
-            <Profile name={"Anna Kim"} src="/assets/images/avatar-anna-kim.webp" activity={"left the group"} status={""} club={"Chess Club"} time={"2 weeks ago"} msg={""}/>
-          </div>
+           
+            <Profile
+              name={data[0].name}
+              src={data[0].src}
+              action={data[0].action}
+              secondary_text={data[0].secondary_text}
+              secondary_text_blue={data[0].secondary_text_blue}
+              time={data[0].time}
+              msg={data[0].msg}
+            />
+           
+            <Profile
+              name={data[1].name}
+              src={data[1].src}
+              action={data[1].action}
+              secondary_text={data[1].secondary_text}
+              secondary_text_blue={data[1].secondary_text_blue}
+              time={data[1].time}
+              msg={data[1].msg}
+            />
+            <Profile
+              name={data[2].name}
+              src={data[2].src}
+              action={data[2].action}
+              secondary_text={data[2].secondary_text}
+              secondary_text_blue={data[2].secondary_text_blue}
+              time={data[2].time}
+              msg={data[2].msg}
+            />
+            <Profile
+              name={data[3].name}
+              src={data[3].src}
+              action={data[3].action}
+              secondary_text={data[3].secondary_text}
+              secondary_text_blue={data[3].secondary_text_blue}
+              time={data[3].time}
+              msg={data[3].msg}
+            />
+            <Profile
+              name={data[4].name}
+              src={data[4].src}
+              action={data[4].action}
+              secondary_text={data[4].secondary_text}
+              secondary_text_blue={data[4].secondary_text_blue}
+              time={data[4].time}
+              src2={data[4].src2}
+              msg={data[4].msg}
+            />
+            <Profile
+              name={data[5].name}
+              src={data[5].src}
+              action={data[5].action}
+              secondary_text={data[5].secondary_text}
+              secondary_text_blue={data[5].secondary_text_blue}
+              time={data[5].time}
+              msg={data[5].msg}
+            />
+            <Profile
+              name={data[6].name}
+              src={data[6].src}
+              action={data[6].action}
+              secondary_text={data[6].secondary_text}
+              secondary_text_blue={data[6].secondary_text_blue}
+              time={data[6].time}
+              msg={data[6].msg}
+            />
+          </div>}
         </div>
       </div>
     </div>
--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,28 +1,37 @@
-const Profile = (props)=>{
-    return(
+import { useState } from "react";
 
-        <div className="profile-1 my-2 flex items-center rounded-md bg-slate-100 py-2">
-              <img
-                src={props.src}
-                className="w-10 mx-5"
-                alt="profile-pic"
-              />
-              <p>
-                <span className="font-bold hover:text-blue-900">{props.name} </span>
-                <span className="text-gray-600 mr-1">{props.activity}</span>
-                <span className="font-bold text-gray-600 hover:text-blue-900">{props.status}</span>
-                <span className="font-bold text-blue-900">{props.club}</span>
-                <br/>
-                <span className="text-gray-600">{props.time}</span>
-              </p>
-              <div className="absolute ml-[565px]">
-              <img src={props.src2} className="w-10"/>
-              </div>            
-            </div>
-            
-            
-        
-    );
+const Profile = (props) => {
+  const [show, setShow] = useState(false);
+  const [color, setColor] = useState('bg-slate-100')
+  const open = () => {
+    setShow(!show);
+    setColor('bg-none');
+  };
+  return (
+    <div className="" onClick={open}>
+      <div className={`profile-1 my-2 flex items-center rounded-md ${color} py-2`}>
+        <img src={props.src} className="w-10 mx-5" alt="profile-pic" />
+        <p>
+          <span className="font-bold hover:text-blue-900">{props.name} </span>
+          <span className="text-gray-600 mr-1">{props.action}</span>
+          <span className="font-bold text-gray-600 hover:text-blue-900">
+            {props.secondary_text}
+          </span>
+          <span className="font-bold text-blue-900">{props.seconday_text_blue}</span>
+          <br />
+          <span className="text-gray-600">{props.time}</span>
+        </p>
+        <div className="absolute ml-[565px]">
+          <img src={props.src2} className="w-10" />
+        </div>
+      </div>
+      {props.msg.length > 0 && show && (
+        <div className="border border-slate-200 ml-16 mr-3 rounded-md break-all hover:bg-slate-100">
+          <h1 className="p-3 text-gray-600">{props.msg}</h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
